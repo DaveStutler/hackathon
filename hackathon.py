2a347de53fca01abeb868689c2c82a459911e8fa
marksoliman3 <mtsoliman@ucdavis.edu>
2023-05-20 14:16:53 -0700

    Update hackathon.py

--- hackathon.py
+++ hackathon.py
@@ -1,1 +1,10 @@
-print("Checking the push")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/api/example', methods=['GET'])
+def example_endpoint():
+    return 'Hello, world! This is the example endpoint.'
+
+if __name__ == '__main__':
+    app.run()
